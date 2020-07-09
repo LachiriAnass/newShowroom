8c19c7c500874f54d4a7c250a67eebab582cd882
LachiriAnass <64714820+LachiriAnass@users.noreply.github.com>
2020-07-10 00:15:11 +0100

    Completing welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Welcome</div>
 
-                <div class="card-body">
-                    Welcome to Showroom
-                </div>
-            </div>
-        </div>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+<ul class="carousel-indicators">
+  <li data-target="#demo" data-slide-to="0" class="active"></li>
+  <li data-target="#demo" data-slide-to="1"></li>
+  <li data-target="#demo" data-slide-to="2"></li>
+</ul>
+
+<!-- The slideshow -->
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="imgs/paintings.jpg" alt="Discover" width="1100" height="500">
+    <div class="carousel-caption">
+      <h1 class="headers">Discover</h1>
+      <p class="para" style="color:blue">Discover new paintings</p>
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="imgs/coast-painting.jpg" alt="Follow" width="1100" height="500">
+    <div class="carousel-caption">
+      <h1 class="headers">Follow</h1>
+      <p class="para" style="color:red">Follow your favorite artists</p>
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="imgs/artist.jpg" alt="Upload" width="1100" height="500">
+    <div class="carousel-caption">
+      <h1 class="headers">Upload</h1>
+      <p class="para" style="color:yellow">Upload your own paintings</p>
+    </div>
+  </div>
+</div>
+
+<!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+    </a>
+</div>
+
+<div class="container pt-3">
+    <div class="row">
+
+    <div class="col-sm-2">
+    </div>
+
+    <div class="col-sm-6">
+    <h3 style="margin-top:5px">Create an account it's super easy !!</h3>  
+    </div>
+
+    <div class="col-sm-4">
+        <a href="/register" class="btn btn-success btn-lg" role="button">Sign up</a>
+     </div>
     </div>
 </div>
+
 @endsection
+
 
